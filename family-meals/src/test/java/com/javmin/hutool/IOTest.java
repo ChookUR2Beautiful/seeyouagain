eda9a09c69187c06a2ec633899b2e8915f770d4e
javmin <641013587@qq.com>
2017-08-18 14:13:08 +0800

    hutool

---
+++ family-meals/src/test/java/com/javmin/hutool/IOTest.java
@@ -0,0 +1,25 @@
+package com.javmin.hutool;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.xiaoleilu.hutool.io.FileUtil;
+import com.xiaoleilu.hutool.io.IoUtil;
+
+public class IOTest {
+	
+	@Test
+	public void IoCopy() throws IOException{
+		BufferedInputStream in = FileUtil.getInputStream("d:/test.txt");
+		BufferedOutputStream out = FileUtil.getOutputStream("d:/test2.txt");
+		long copySize = IoUtil.copy(in, out, IoUtil.DEFAULT_BUFFER_SIZE);
+	}
+	
+	
+	
+	
+	
+}
